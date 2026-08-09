9bab13c4fdc293075feb3738aaafd954d74206fa
dobi10 <dozohi1029@gmail.com>
2026-08-09 12:17:37 +0300

    Refactor Firebase imports and formatting

--- firebase.js
+++ firebase.js
@@ -1,29 +1,49 @@
-import { initializeApp } from "firebase/app";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
 import {
     getAuth
-} from "firebase/auth";
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 import {
     getFirestore
-} from "firebase/firestore";
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAu8vCNwhZ8irI2Knm7Ssc7Vku2f9XCFtc",
-    authDomain: "brightbridge-0.firebaseapp.com",
-    projectId: "brightbridge-0",
-    storageBucket: "brightbridge-0.firebasestorage.app",
-    messagingSenderId: "809001297248",
-    appId: "1:809001297248:web:4b23e504ef9826bcdcea05"
+
+    apiKey:
+        "AIzaSyAu8vCNwhZ8irI2Knm7Ssc7Vku2f9XCFtc",
+
+    authDomain:
+        "brightbridge-0.firebaseapp.com",
+
+    projectId:
+        "brightbridge-0",
+
+    storageBucket:
+        "brightbridge-0.firebasestorage.app",
+
+    messagingSenderId:
+        "809001297248",
+
+    appId:
+        "1:809001297248:web:4f03dc77249b7781dcea05"
+
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
 
-const db = getFirestore(app);
+const auth =
+    getAuth(app);
+
+
+const db =
+    getFirestore(app);
 
 
 export {
